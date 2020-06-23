a7a8019f0be36ba6a183bab0ad27886d763297da
vamshi-tg <vamshi.tg@beautifulcode.in>
2020-06-23 23:26:08 +0530

    Setup Webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'sudoku_solver.css'
+    }
+    )
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js(x?)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', "@babel/preset-react"]
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    // Resolve in this order
+    extensions: ['*', '.js', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, './src/')
+    }
+  },
+  devtool: 'cheap-module-eval-source-map',
+  // changed line
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+};
+
