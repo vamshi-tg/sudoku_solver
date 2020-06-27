7e9c0dd7e238d2ea265b1045cada156d663f5d1b
vamshi-tg <vamshi.tg@beautifulcode.in>
2020-06-27 20:43:50 +0530

    Add number picker

--- src/components/Board/BoardItem.jsx
+++ src/components/Board/BoardItem.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Popup from 'reactjs-popup';
+
+const NumberPicker = () => {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  return numbers.map((number, index) => {
+    return (
+      <div key={index} className="cell">
+        {number}
+      </div>
+    );
+  });
+};
 
 const BoardItem = (props) => {
   const id = `${props.rowIndex}, ${props.colIndex}`;
-  return (
-    <div className="board-item" data-pos={`[${id}]`} onClick={() => window.alert(`Clicked ${id}`)}>
+  const item = (
+    <div className="board-item" data-pos={`[${id}]`}>
       {props.value}
     </div>
+  );
+  return (
+    <Popup trigger={item} modal closeOnDocumentClick>
+      {(close) => (
+        <div className="number-picker-modal">
+          <a className="close" onClick={close}>
+            &times;
+          </a>
+          <div className="container">
+            <NumberPicker />
+          </div>
+        </div>
+      )}
+    </Popup>
   );
 };
 
